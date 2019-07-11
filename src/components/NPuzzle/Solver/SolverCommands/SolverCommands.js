c1cb1d102c60fd8288c2ea5feb7261f12212b992
m-conti <mc.heux@gmail.com>
2019-07-11 22:01:53 +0200

    radio

---
+++ src/components/NPuzzle/Solver/SolverCommands/SolverCommands.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+
+import classes from './SolverCommands.less';
+
+import Radio from '../../../UI/Radio/Radio';
+
+const SolverCommands = (props) => {
+
+	props
+
+	return (
+		<div className={classes.SolverCommand}>
+
+		</div>
+	);
+};
+
+export default SolverCommands;
