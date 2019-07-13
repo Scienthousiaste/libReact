f1e72296226dd6bb9af1a585f17068033f6314ef
Timothee BEHRA <tbehra@student.42.fr>
2019-07-13 12:14:29 +0200

    1 line array parsing

--- src/components/NPuzzle/PuzzleCommands/PuzzleParser/PuzzleParser.js
+++ src/components/NPuzzle/PuzzleCommands/PuzzleParser/PuzzleParser.js
@@ -1,9 +1,6 @@
 import {MAX_PUZZLE_SIZE} from '../../../../helpers/Npuzzle/defines';
-
 import React, {useState} from 'react';
-
 import classes from './PuzzleParser.less';
-
 import Input from '../../../UI/Input/Input';
 import Button from '../../../UI/Button/Button';
 
@@ -27,17 +24,32 @@ const PuzzleParser = (props) => {
 		}
 		if (arr.length > 0) {
 			let candidateSize;
+			let arrNumbers = [];
+			let numbersBingo = [];
 
-			//TODO : ca
 			if (arr.length == 1) {
 				let numbers = arr[0].split(' ');
-					
 				candidateSize = Math.sqrt(numbers.length);
 				if (candidateSize - Math.floor(candidateSize) !== 0) {
 					displayMessage("incorrect number of values in a single row");
 					return;
 				}
-				//TODO: finir parsing ici
+				const numRegex = /(?:(\d+)[, ]*)/g;
+				numbersBingo = Array(candidateSize * candidateSize).fill(false);
+
+				let r;
+				while ((r = numRegex.exec(arr[0])) !== null) {
+					let n = parseInt(r[0]);
+					if (numbersBingo[n] !== false) {
+						console.log(numbersBingo);
+						displayMessage("A value is out of range or doubled");
+						return;
+					}
+					else {
+						numbersBingo[n] = true;
+						arrNumbers.push(n);
+					}
+				}
 			}
 
 			else {
@@ -47,41 +59,40 @@ const PuzzleParser = (props) => {
 				} else {
 					candidateSize = arr.length;
 				}
-			}
 
-			if (isNaN(candidateSize)) {
-				displayMessage("Couldn't parse a candidate size - parsing failed");
-				return;
-			}
-			if (candidateSize < 2) {
-				displayMessage("The candidate size is inferiror to 2, but the npuzzle size must be at least 2");
-				return;
-			}
-			if (candidateSize > MAX_PUZZLE_SIZE) {
-				displayMessage("The detected size of the puzzle is too large (superior to " + MAX_PUZZLE_SIZE + ")");
-				return;
-			}
-
-			let arrNumbers = [];
-			let numbersBingo = Array(candidateSize * candidateSize).fill(false);
-			for (let i = 0; i < arr.length; i++) {
-				arr[i] = arr[i].split("#")[0].trim();
-
-				let numbers = arr[i].split(/\s/).filter((part) => {
-					if (part.indexOf("#") === 0) return false;
-					return !!part;
-				}).map(x => parseInt(x));
-				if (numbers.length !== candidateSize) {
-					displayMessage("The " + (i + 1) + "th row doesn't have the correct number of values");
+				if (isNaN(candidateSize)) {
+					displayMessage("Couldn't parse a candidate size - parsing failed");
 					return;
 				}
-				arrNumbers.push(...numbers);
-				for (let j = 0; j < numbers.length; j++) {
-					if (numbersBingo[numbers[j]] !== false) {
-						displayMessage("A value is out of range or doubled");
+				if (candidateSize < 2) {
+					displayMessage("The candidate size is inferiror to 2, but the npuzzle size must be at least 2");
+					return;
+				}
+				if (candidateSize > MAX_PUZZLE_SIZE) {
+					displayMessage("The detected size of the puzzle is too large (superior to " + MAX_PUZZLE_SIZE + ")");
+					return;
+				}
+
+				numbersBingo = Array(candidateSize * candidateSize).fill(false);
+				for (let i = 0; i < arr.length; i++) {
+					arr[i] = arr[i].split("#")[0].trim();
+
+					let numbers = arr[i].split(/\s/).filter((part) => {
+						if (part.indexOf("#") === 0) return false;
+						return !!part;
+					}).map(x => parseInt(x));
+					if (numbers.length !== candidateSize) {
+						displayMessage("The " + (i + 1) + "th row doesn't have the correct number of values");
 						return;
 					}
-					numbersBingo[numbers[j]] = true;
+					arrNumbers.push(...numbers);
+					for (let j = 0; j < numbers.length; j++) {
+						if (numbersBingo[numbers[j]] !== false) {
+							displayMessage("A value is out of range or doubled");
+							return;
+						}
+						numbersBingo[numbers[j]] = true;
+					}
 				}
 			}
 
